a9777e25cdf8d6637f7b6e7c6ab9b266e87c8b2c
Benoit Fruchard <bfruchar@student.42.fr>
2017-11-23 11:18:27 +0100

    main to start

---
+++ asm/main.c
@@ -0,0 +1,16 @@
+#include "../libft/libft.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		ft_putstr("Usage: ./asm [-a] <sourcefile.s>\n");
+		return (0);
+	}
+	else
+	{
+//		launch_compilation(argv[1]);
+		return (1);
+	}
+	return (0);
+}
