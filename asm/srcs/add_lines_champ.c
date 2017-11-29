da0311dab8fcb24cb0f9d4f0500503a103977726
Benoit FRUCHARD <bfruchar@e1r2p1.42.fr>
2017-11-29 15:20:22 +0100

    complete structure

--- asm/srcs/add_lines_champ.c
+++ asm/srcs/add_lines_champ.c
@@ -6,11 +6,60 @@
 /*   By: bfruchar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 16:24:32 by bfruchar          #+#    #+#             */
-/*   Updated: 2017/11/29 11:13:14 by bfruchar         ###   ########.fr       */
+/*   Updated: 2017/11/29 15:19:53 by bfruchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/asm.h"
+
+//je verifie si il s agit d un label
+int			get_two_points(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == ':' && str[i + 1] == '\0')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+//je rajoute le nom dans la liste
+void		add_name_list(t_champ *list)
+{
+	int i;
+	int j;
+	char *str;
+
+	i = 0;
+	while (list->line[i] == ' ' || list->line[i] == '\t')
+		i++;
+	j = i;
+	while (list->line[j] != ' ' && list->line[j] != '\t' && list->line[j] != ',')
+		j++;
+	str = ft_strnew(j - i + 1);
+	str = ft_strncpy(str, &list->line[i], (j - i));
+	list->name = ft_strdup(str);
+	if (get_two_points(list->name))
+		list->is_label = 1;
+	else
+		list->is_label = 0;
+	if (str)
+		free(str);
+}
+
+void		add_infos_list(t_champ **begin)
+{
+	t_champ	*list;
+
+	list = *begin;
+	while (list->next)
+		list = list->next;
+	add_name_list(list);
+}
 
 t_champ		*ft_lstnew_line(char *line)
 {
@@ -41,4 +90,5 @@ void		ft_lstadd_lines(t_champ **begin, char *line, int i)
 		list->prev = tmp;
 		list->position = i;
 	}
+	add_infos_list(begin);
 }
