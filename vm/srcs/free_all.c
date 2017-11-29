5a1c30e3280b1c0bc317adb8aeff546541de534c
VBaudron <v.baudron@student.42.fr>
2017-11-29 13:24:35 +0100

    Create Arena : Done EXCEPT t_opt - LEAKS 0

--- vm/srcs/free_all.c
+++ vm/srcs/free_all.c
@@ -10,6 +10,83 @@ void	free_fds(int **fds)
 	free(*fds);
 }
 
+void	free_players(t_play **players, int nb_players)
+{
+	int	i;
+
+	i = 0;
+	if (players)
+	{
+		while (i < nb_players)
+		{
+			if (players[i])
+			{
+				if (players[i]->name)
+					free(players[i]->name);
+				players[i]->name = NULL;
+				if (players[i]->comment)
+					free(players[i]->comment);
+				players[i]->comment = NULL;
+				if (players[i]->body)
+					free(players[i]->body);
+				players[i]->body = NULL;
+				free(players[i]);
+				players[i] = NULL;
+			}
+			i++;
+		}
+		free(players);
+	}
+	players = NULL;
+}
+
+void	free_bdd(t_bdd **bdd)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (bdd)
+	{
+		while (i < 16)
+		{
+			free(bdd[i]->name);
+			bdd[i]->name = NULL;
+			free(bdd[i]->meaning);
+			bdd[i]->meaning = NULL;
+//			ft_printf("nb_ocp to free dans bdd[%d] = %d\n", i, bdd[i]->nb_ocp);
+			while (j < bdd[i]->nb_ocp)
+			{
+				bdd[i]->ocp[j]->fct = NULL;
+//				ft_printf("free : bbd[%d]->ocp[%d]\n", i, j);
+				free(bdd[i]->ocp[j]);
+				bdd[i]->ocp[j] = NULL;
+				j++;
+			}
+			ft_putchar('\n');
+			free(bdd[i]->ocp);
+			bdd[i]->ocp = NULL;
+			free(bdd[i]);
+			i++;
+			j = 0;
+		}
+		free(bdd);
+		bdd = NULL;
+	}
+}
+
+void	free_list_proc(t_proc_base *list)
+{
+	if (list)
+	{
+		while (list->first != NULL)
+			kill_process(list->first, list);
+		free(list);
+		list = NULL;
+	}
+}
+
 void	free_arena(t_arena **arena)
 {
 	if (!arena || !*arena)
@@ -18,5 +95,11 @@ void	free_arena(t_arena **arena)
 		free_opts(&(*arena)->opts);
 	if ((*arena)->fds)
 		free_fds(&(*arena)->fds);
+	if ((*arena)->players)
+		free_players((*arena)->players, (*arena)->nb_players);
+	if ((*arena)->bdd)
+		free_bdd((*arena)->bdd);
+	if ((*arena)->list_proc)
+		free_list_proc((*arena)->list_proc);
 	free(*arena);
 }
