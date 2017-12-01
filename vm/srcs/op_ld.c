bcdc9a8570b270d03781a3b84c5cc2062fec62a2
Dany ALAWWA <dalawwa@e1r7p6.42.fr>
2017-12-01 15:41:01 +0100

    Fix a_hexa_to_i for d_value

--- vm/srcs/op_ld.c
+++ vm/srcs/op_ld.c
@@ -4,7 +4,8 @@ int		op_ld(t_arena *arena, t_exe *exe)
 {
 	if (exe->arg1->type == 'd')
 	{
-		if (exe->arg1->d_value > 0xfff)
+		// verifier s'il faut pas % 0x10000
+		if (exe->arg1->d_value > 0xffff)
 			return (0);
 		exe->process->reg[exe->arg2->d_value][0] = exe->arg1->value[2];
 		exe->process->reg[exe->arg2->d_value][1] = exe->arg1->value[3];
