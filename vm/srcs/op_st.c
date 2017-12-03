4c78ba939dd03859eefaddb7a7b0387e4d2e8ac3
VBaudron <v.baudron@student.42.fr>
2017-12-03 12:33:22 +0100

    OP in progress

--- vm/srcs/op_st.c
+++ vm/srcs/op_st.c
@@ -5,14 +5,16 @@ int		op_st(t_arena *arena, t_exe *exe)
 	// a tester avec la vraie vm
 	if (exe->arg2->type == 'r')
 	{
-		exe->process->reg[exe->arg1->d_value][0] = exe->process->reg[exe->arg2->d_value][0];
-		exe->process->reg[exe->arg1->d_value][1] = exe->process->reg[exe->arg2->d_value][1];
+		exe->process->reg[exe->arg2->d_value][0] = exe->process->reg[exe->arg1->d_value][0];
+		exe->process->reg[exe->arg2->d_value][1] = exe->process->reg[exe->arg1->d_value][1];
 	}
 	else
 	{
-		exe->process->reg[exe->arg1->d_value][0] = 0;
-		exe->process->reg[exe->arg1->d_value][1] = arena->mem[exe->process->pc + (exe->arg2->d_value % IDX_MOD)];
+		arena->mem[exe->process->pc + (exe->arg2->d_value % IDX_MOD)] = exe->process->reg[exe->arg1->d_value][0];
 	}
 	is_carry_to_modify(exe);
 	return (1);
 }
+
+/* PB : Dans else : mem = 1 char et reg = 2 char mais on doit mettre reg value dans mem[arg1]
+ */
