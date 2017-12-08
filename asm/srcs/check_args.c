ad2a47a42c83c8b4e5bc367d08259faad1b20a1b
Benoit FRUCHARD <bfruchar@e1r1p3.42.fr>
2017-12-08 12:11:12 +0100

    progress to get a clean struct with good data

--- asm/srcs/check_args.c
+++ asm/srcs/check_args.c
@@ -6,17 +6,23 @@
 /*   By: bfruchar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 09:21:43 by bfruchar          #+#    #+#             */
-/*   Updated: 2017/12/06 15:44:42 by bfruchar         ###   ########.fr       */
+/*   Updated: 2017/12/08 11:57:09 by bfruchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/asm.h"
 
-int		check_is_direct(char *str)
+int		check_is_direct(char *str, t_champ *list, int num)
 {
 	int	i;
 
 	i = 1;
+	if (num == 1)
+		list->size_param1 = 4;
+	else if (num == 2)
+		list->size_param2 = 4;
+	else if (num == 3)
+		list->size_param3 = 4;
 	if (str[0] != '%')
 		return (0);
 	if (str[i] == ':')
@@ -32,13 +38,17 @@ int		check_is_direct(char *str)
 	return (1);
 }
 
-int		check_is_indirect(char *str)
+int		check_is_indirect(char *str, t_champ *list, int num)
 {
 	int	i;
 
-	i = 1;
-	if (str[0] != '%')
-		return (0);
+	i = 0;
+	if (num == 1)
+		list->size_param1 = 2;
+	else if (num == 2)
+		list->size_param2 = 2;
+	else if (num == 3)
+		list->size_param3 = 2;
 	if (str[i] == ':')
 		return (1);
 	if (str[i] == '-' || str[i] == '+')
@@ -52,7 +62,7 @@ int		check_is_indirect(char *str)
 	return (1);
 }
 
-int		check_is_reg(char *str)
+int		check_is_reg(char *str, t_champ *list, int num)
 {
 	int		i;
 
@@ -61,6 +71,12 @@ int		check_is_reg(char *str)
 	i = ft_atoi(&str[1]);
 	if (i > 16 || i < 1)
 		return (0);
+	if (num == 1)
+		list->size_param1 = 1;
+	else if (num == 2)
+		list->size_param2 = 1;
+	else if (num == 3)
+		list->size_param3 = 1;
 	return (1);
 }
 
@@ -68,26 +84,27 @@ int		check_is_reg(char *str)
 //on va verifier pour chaque ligne si les arguments recus sont valides
 //il faudrait checker aussi que la suite de la phrase est coherente
 //il faudrait verfiier qu il n y a pas plus d arguments
-int		check_args_valid(char *str, int op)
+int		check_args_valid(char *str, int op, t_champ *list)
 {
 	char	**tab;
 
+	list->params = ft_strsplit_three(str, ' ', '\t', ',');
 	tab = ft_strsplit_three(str, ' ', '\t', ',');
-	if (tab[1] && (op == 1 || op == 9 || op == 15 || op == 12) && (check_is_direct(tab[1])))
+	if (tab[1] && (op == 1 || op == 9 || op == 15 || op == 12) && (check_is_direct(tab[1], list, 1)))
 		return (1);
-	else if (tab[1] && tab[2] && (op == 2 || op == 13) && (check_is_indirect(tab[1]) || check_is_direct(tab[1])) && (check_is_reg(tab[2])))
+	else if (tab[1] && tab[2] && (op == 2 || op == 13) && (check_is_indirect(tab[1], list, 1) || check_is_direct(tab[1], list, 1)) && (check_is_reg(tab[2], list, 2)))
 		return (1);
-	else if (tab[1] && tab[2] && op == 3 && check_is_reg(tab[1]) && (check_is_reg(tab[2]) || check_is_indirect(tab[2])))
+	else if (tab[1] && tab[2] && op == 3 && check_is_reg(tab[1], list, 1) && (check_is_reg(tab[2], list, 2) || check_is_indirect(tab[2], list, 2)))
 		return (1);
-	else if (tab[1] && tab[2] && tab[3] && (op == 4 || op == 5) && check_is_reg(tab[1]) && check_is_reg(tab[2]) && check_is_reg(tab[3]))
+	else if (tab[1] && tab[2] && tab[3] && (op == 4 || op == 5) && check_is_reg(tab[1], list, 1) && check_is_reg(tab[2], list, 2) && check_is_reg(tab[3], list, 3))
 		return (1);
-	else if (tab[1] && tab[2] && tab[3] && (op == 6 || op == 7 || op == 8) && (check_is_reg(tab[1]) || check_is_indirect(tab[1]) || check_is_direct(tab[1])) && (check_is_reg(tab[2]) || check_is_indirect(tab[2]) || check_is_direct(tab[2])) && check_is_reg(tab[3]))
+	else if (tab[1] && tab[2] && tab[3] && (op == 6 || op == 7 || op == 8) && (check_is_reg(tab[1], list, 1) || check_is_indirect(tab[1], list, 1) || check_is_direct(tab[1], list, 1)) && (check_is_reg(tab[2], list, 2) || check_is_indirect(tab[2], list, 2) || check_is_direct(tab[2], list, 2)) && check_is_reg(tab[3], list, 3))
 		return (1);
-	else if (tab[1] && tab[2] && tab[3] && (op == 10 ||op == 14) && (check_is_reg(tab[1]) || check_is_indirect(tab[1]) || check_is_direct(tab[1])) && (check_is_reg(tab[2]) || check_is_direct(tab[2])) && check_is_reg(tab[3]))
+	else if (tab[1] && tab[2] && tab[3] && (op == 10 ||op == 14) && (check_is_reg(tab[1], list, 1) || check_is_indirect(tab[1], list, 1) || check_is_direct(tab[1], list, 1)) && (check_is_reg(tab[2], list, 2) || check_is_direct(tab[2], list, 2)) && check_is_reg(tab[3], list, 3))
 		return (1);
-	else if (tab[1] && tab[2] && tab[3] && op == 11 && check_is_reg(tab[1]) && (check_is_reg(tab[2]) || check_is_indirect(tab[2]) || check_is_direct(tab[2])) && (check_is_reg(tab[3]) || check_is_direct(tab[3])))
+	else if (tab[1] && tab[2] && tab[3] && op == 11 && check_is_reg(tab[1], list, 1) && (check_is_reg(tab[2], list, 2) || check_is_indirect(tab[2], list, 2) || check_is_direct(tab[2], list, 2)) && (check_is_reg(tab[3], list, 3) || check_is_direct(tab[3], list, 3)))
 		return (1);
-	else if (tab[1] && op == 16 && check_is_reg(tab[1]))
+	else if (tab[1] && op == 16 && check_is_reg(tab[1], list, 1))
 		return (1);
 	return (0);
 }
--- asm/srcs/parsing2_asm.c
+++ asm/srcs/parsing2_asm.c
@@ -6,7 +6,7 @@
 /*   By: bfruchar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 17:45:13 by bfruchar          #+#    #+#             */
-/*   Updated: 2017/12/01 17:44:27 by bfruchar         ###   ########.fr       */
+/*   Updated: 2017/12/08 11:40:54 by bfruchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ void	get_champ_data(t_champ *champ, int fd)
 	}
 	while (champ->next)
 	{
-		ft_printf("%s %i nb_args: %i\n", champ->name, champ->op_code, champ->nb_params);
+		ft_printf("LINE: %s. NAME: %s. SIZE PARAMS: %i %i %i POSITION: %i\n", champ->line, champ->name, champ->size_param1, champ->size_param2, champ->size_param3, champ->position);
 		champ = champ->next;
 	}
 	return ;
