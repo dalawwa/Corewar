54b307a45c3a5adb296fd781a431b21f0726c70b
Dany ALAWWA <dalawwa@e1r12p16.42.fr>
2017-12-09 20:18:28 +0100

    Verbosity levels done

--- vm/srcs/op_live.c
+++ vm/srcs/op_live.c
@@ -5,6 +5,8 @@ int		op_live(t_arena *arena, t_exe *exe)
 	int	i;
 
 	i = 0;
+	if (arena->opts->is_v1 && exe->bdd_op->opcode == 1)
+			ft_printf("Player %d (%s) is said to be alive\n", exe->process->player->play_num, exe->process->player->name);
 	print_exe_opts(arena, exe);
 	exe->process->nb_live++;
 	arena->list_proc->nb_live_total++;
