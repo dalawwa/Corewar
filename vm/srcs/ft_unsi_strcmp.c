b8f15e0d93b4494721d5035dd4817f50ffc7ed12
Dany ALAWWA <dalawwa@e1r7p6.42.fr>
2017-12-11 11:14:51 +0100

    Fix op_live

---
+++ vm/srcs/ft_unsi_strcmp.c
@@ -0,0 +1,16 @@
+#include "corewar.h"
+
+int		ft_unsi_strcmp(unsigned char *s1, unsigned char *s2)
+{
+	unsigned char *ss1;
+	unsigned char *ss2;
+
+	ss1 = s1;
+	ss2 = s2;
+	while (ss1 && ss2 && *ss1 == *ss2)
+	{
+		ss1++;
+		ss2++;
+	}
+	return (*ss2 - *ss1);
+}
