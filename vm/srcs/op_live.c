15638d0643b573ad7f1e9592a42bc4133fa86a40
Dany ALAWWA <dalawwa@e1r4p8.42.fr>
2018-01-26 09:31:45 +0100

    Fix live verbosity

--- vm/srcs/op_live.c
+++ vm/srcs/op_live.c
@@ -17,7 +17,8 @@ int		op_live(t_arena *arena, t_exe *exe)
 	{
 		// changement de logique car live reagit a play_live_num et pas a play_num
 //		if (arena->players[i]->play_num == exe->arg1->d_value)
-		if (!ft_unsi_strcmp(arena->players[i]->play_live_num, exe->arg1->data))
+//		if (!ft_unsi_strcmp(arena->players[i]->play_live_num, exe->arg1->data))
+		if (a_hexa_to_i(exe->arg1->data, 4) == a_hexa_to_i(arena->players[i]->play_live_num, 4))
 		{
 			arena->last_player_alive = arena->players[i];
 			if (arena->opts->is_v1)
