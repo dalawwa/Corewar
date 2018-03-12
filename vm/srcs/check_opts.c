f3cde5378295b5b51946751813f91dd8c5459589
VBaudron <v.baudron@student.42.fr>
2018-03-12 16:01:06 +0100

    clean opts

--- vm/srcs/check_opts.c
+++ vm/srcs/check_opts.c
@@ -41,97 +41,14 @@ int		init_opts(t_arena **arena)
 	return (1);
 }
 
-static void	set_opt_flag(t_opt *opts, int flag, int val)
-{
-	if (flag == 1)
-	{
-		opts->has_d = 1;
-		opts->d = val;
-	}
-	if (flag == 2)
-	{
-		opts->has_s = 1;
-		opts->s = val;
-	}
-	if (flag == 3)
-	{
-		opts->has_v = 1;
-		opts->v = (char)val;
-		set_v_values(opts, (char)val);
-	}
-}
-
-void	set_v_values(t_opt *opts, int val)
-{
-	if (val > 31)
-		val = val % 32;
-	opts->is_v0 = 1;
-	opts->is_v1 = val & 1;
-	opts->is_v2 = val & 2;
-	opts->is_v4 = val & 4;
-	opts->is_v8 = val & 8;
-	opts->is_v16 = val & 16;
-}
-
-static int		check_opts_flags(t_opt *opts, int *flag_indx, int ac, char **av)
-{
-	if (flag_indx[0] < 4 && flag_indx[0] > 0 && flag_indx[1] + 1 >= ac)
-	{
-		ft_printf("Can't read source file %s\n", av[flag_indx[1]]);
-		return (0);
-	}
-	if (flag_indx[0] < 6 && flag_indx[0] > 0)
-	{
-		if (flag_indx[1] + 1 < ac && flag_indx[0] < 4)
-		{
-			flag_indx[1]++;
-			set_opt_flag(opts, flag_indx[0], ft_atoi(av[flag_indx[1]]));
-			flag_indx[0] = 0;
-		}
-		else if (flag_indx[0] == 4)
-		{
-			opts->a_stealth = 0;
-			opts->has_a = 1;
-			if (flag_indx[1] + 1 < ac && !ft_strcmp(av[flag_indx[1] + 1], "--stealth"))
-			{
-				opts->a_stealth = 1;
-				flag_indx[1]++;
-			}
-		}
-		else if (flag_indx[0] == 5)
-		{
-			opts->b_stealth = 0;
-			opts->has_b = 1;
-			if (flag_indx[1] + 1 < ac && !ft_strcmp(av[flag_indx[1] + 1], "--stealth"))
-			{
-				opts->b_stealth = 1;
-				flag_indx[1]++;
-			}
-			flag_indx[0] = 0;
-		}
-		else
-		{
-			ft_printf("Can't read source file %s\n", av[flag_indx[1]]);
-			return (0);
-		}
-	}
-	return (1);
-}
-
 static int			check_opts_fds(t_file *file, int *flag_indx, char **av)
 {
-//	opts->files[i] = (t_file *)malloc(sizeof(t_file));
-//	if (opts->files[i] == NULL)
-//		return (perror_int("Error ", 0));
 	file->name = addstr(av[flag_indx[1]]);
 	if ((file->fd = open(av[flag_indx[1]], O_RDONLY)) == -1)
 	{
 		ft_printf("Can't read source file %s\n", file->name);
 		return (0);
 	}
-//	else
-//		append_fds_tab(opts, opts->fds_nb, flag_indx[0]);
-//	opts->fds_nb++;
 	flag_indx[0] = 0;
 	return (1);
 }
@@ -158,17 +75,13 @@ int		init_files(t_arena *arena)
 
 int		check_opts(t_arena *arena, int ac, char **av)
 {
-	// free_leaks;
 	int		flag_indx[2];
 
 	flag_indx[1] = 1;
-	if (!(init_opts(&arena)))
-		return (0);
-	if (init_files(arena) == 0)
+	if (!(init_opts(&arena)) || init_files(arena) == 0)
 		return (0);
 	while (flag_indx[1] < ac)
 	{
-//		ft_printf("flag_indx[1] = %d - ac = %d arena->nb_players = %d\n", flag_indx[1], ac, arena->nb_players);
 		flag_indx[0] = is_flag(av[flag_indx[1]]);
 		if (flag_indx[0] < 6 && flag_indx[0] > 0)
 		{
@@ -179,20 +92,11 @@ int		check_opts(t_arena *arena, int ac, char **av)
 		{
 			arena->nb_players++;
 			if (arena->nb_players > 4)
-			{
-				ft_putendl("Error : Too many champions");
-				return (0);
-			}
+				return(perror_int("Error : Too many champions", 0));
 			if (!(check_opts_fds(arena->files[arena->nb_players - 1], flag_indx, av)))
 				return (0);
 		}
 		flag_indx[1]++;
-	}
-//	arena->nb_players++;
-	if (arena->nb_players == 0)
-	{
-		ft_putendl("Print Usage here");
-		return (0);
 	}
 	return (1);
 }
