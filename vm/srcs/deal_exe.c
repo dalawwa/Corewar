af9c42b15f13367464f4cd8899cbe1b30caa9fb5
Benoit FRUCHARD <bfruchar@e1r12p15.42.fr>
2018-03-15 14:50:33 +0100

    add -b -> print process

--- vm/srcs/deal_exe.c
+++ vm/srcs/deal_exe.c
@@ -6,11 +6,37 @@
 /*   By: bfruchar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/15 12:26:33 by bfruchar          #+#    #+#             */
-/*   Updated: 2018/03/15 12:27:12 by bfruchar         ###   ########.fr       */
+/*   Updated: 2018/03/15 14:49:05 by bfruchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+
+void		print_regs(t_proc *process)
+{
+	int	i;
+
+	i = 1;
+	while (i <= REG_NUMBER)
+	{
+		ft_printf("reg[%d] = %.2x%.2x %.2x%.2x", i, process->reg[i][0],
+				process->reg[i][1], process->reg[i][2], process->reg[i][3]);
+		i++;
+		if (i <= REG_NUMBER)
+			ft_putstr(" - ");
+	}
+	ft_putchar('\n');
+}
+
+void		print_process(t_proc *process)
+{
+	ft_putendl("__Current PROCESS");
+	ft_printf("Process num %d :\nCarry = %d - pc = %d\n",
+			process->process_num,
+			process->carry, process->pc);
+	print_regs(process);
+	ft_putendl("__END");
+}
 
 void		execute_exe(t_arena *arena, t_proc *elem)
 {
@@ -23,7 +49,11 @@ void		execute_exe(t_arena *arena, t_proc *elem)
 	if (elem->exe_op->ocp_op != NULL)
 	{
 		if (elem->exe_op->ocp_op->fct != NULL)
+		{
 			elem->exe_op->ocp_op->fct(arena, elem->exe_op);
+			if (arena->opts->has_b)
+				print_process(elem);
+		}
 		if (elem->exe_op->opcode != 9)
 			inc_pc(elem, elem->exe_op->ocp_op->size_adv);
 	}
--- vm/srcs/print_usage.c
+++ vm/srcs/print_usage.c
@@ -6,7 +6,7 @@
 /*   By: vbaudron <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/13 10:53:49 by vbaudron          #+#    #+#             */
-/*   Updated: 2018/03/15 13:03:24 by bfruchar         ###   ########.fr       */
+/*   Updated: 2018/03/15 14:35:32 by bfruchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ int	print_usage(void)
 	ft_putstr("#### TEXT OUTPUT MODE ");
 	ft_putstr("##########################################################\n");
 	ft_putstr("\t-d N\t\t: Dumps memory after N cycles then exits\n");
-	ft_putstr("\t-s N\t\t: Runs N cycles, dumps memory, pauses, then ");
+	ft_putstr("\t-b\t\t: Show process after OP\n");
+	ft_putstr("\t-s N\t\t: Runs N cycles, dumps memory, then ");
 	ft_putstr("repeats\n\t-v N\t\t: Verbosity levels, can be added together");
 	ft_putstr(" to enable several\n\t\t\t\t- 0 : Show only essentials\n");
 	ft_putstr("\t\t\t\t- 1 : Show lives\n");
@@ -28,15 +29,8 @@ int	print_usage(void)
 	ft_putstr("\t\t\t\t- 4 : Show operations (Params are NOT litteral ...)\n");
 	ft_putstr("\t\t\t\t- 8 : Show deaths\n");
 	ft_putstr("\t\t\t\t- 16 : Show PC movements (Except for jumps)\n");
-	ft_putstr("#### BINARY OUTPUT MODE ");
-	ft_putstr("##########################################################\n");
-	ft_putstr("\t-b\t\t: Binary output mode for corewar.42.fr\n");
-	ft_putstr("\t--stealth\t: Hides the real contents of the memory\n");
 	ft_putstr("#### NCURSES OUTPUT MODE ");
 	ft_putstr("##########################################################\n");
 	ft_putstr("\t-n\t\t: Ncurses output mode\n");
-	ft_putstr("\t--stealth\t: Hides the real contents of the memory\n");
-	ft_putstr("###########################################");
-	ft_putstr("#####################################\n");
 	return (0);
 }
